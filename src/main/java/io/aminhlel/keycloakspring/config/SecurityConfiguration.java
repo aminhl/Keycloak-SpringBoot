86562fbfa64bc867b38097a26be19ab7b0db6c5e
aminhl <amin.hlel@esprit.tn>
2024-03-30 21:31:58 +0100

    update: update securityconfig syntax

--- src/main/java/io/aminhlel/keycloakspring/config/SecurityConfiguration.java
+++ src/main/java/io/aminhlel/keycloakspring/config/SecurityConfiguration.java
@@ -1,25 +1,36 @@
 package io.aminhlel.keycloakspring.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .csrf()
+                    .disable()
+                .authorizeHttpRequests()
+                    .anyRequest()
+                        .authenticated();
+        http
+                .oauth2ResourceServer()
+                    .jwt()
+                        .jwtAuthenticationConverter(jwtAuthConverter);
+        http
+                .sessionManagement()
+                     .sessionCreationPolicy(STATELESS);
         return http.build();
     }
 }
